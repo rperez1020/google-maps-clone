4bac5f3229ab1dc4a95d6b8ae978b35545b67a02
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2023-12-17 23:50:18 -0800

    update

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicnBlcmV6MTAyMCIsImEiOiJjbHFhOTdkb2cxems5MmtteHRzNjhjY3U4In0.PHLgJOGUrEKvUvyWti8Gfg'
 
-var map = new mapboxgl.Map({
-    accessToken: MAPBOX_ACCESS_TOKEN,
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
-
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy:true})
 
+function setupMap(centerPosition){
+    const map = new mapboxgl.Map({
+        accessToken: MAPBOX_ACCESS_TOKEN,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: centerPosition,
+        zoom: 15
+    });
+    const navigationControls = new mapboxgl.NavigationControl()
+    map.addControl(navigationControls)
+
+    const directionControls = new MapboxDirections({
+        accessToken: MAPBOX_ACCESS_TOKEN
+    })
+    map.addControl(directionControls, 'top-left')
+}
+
 function successLocation(position){
-    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
 }
 
 function errorLocation(){
-    console.log("error")
+    setupMap([2.24, 53.48])
 }
